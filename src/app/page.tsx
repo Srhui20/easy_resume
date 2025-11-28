644a5d2dc01e049f4812ad0b0f1d4da827eb0299
Srhui20 <2874744820@qq.com>
2025-11-28 17:13:47 +0800

    test: test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 const a = 1;
 
+const b = 1;
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
