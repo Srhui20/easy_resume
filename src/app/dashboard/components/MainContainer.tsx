b7940f409bd1b222ba90f3919a3c8f918e9c9823
Srhui20 <2874744820@qq.com>
2025-12-02 22:53:47 +0800

    feat:  页面元素移动&添加上方和左方边界

--- src/app/dashboard/components/MainContainer.tsx
+++ src/app/dashboard/components/MainContainer.tsx
@@ -1,46 +1,73 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import resumeStyle1 from "@/lib//resume_sytle/resume1";
 import { usePublicStore } from "@/lib/store/public";
 import { useMousePosition, useZoom } from "@/lib/userMouseHook";
-import type { PAGE_ATTRIBUTE } from "@/types/resume";
 import styles from "./index.module.scss";
 
 export default function MainContainer() {
   const { scale } = useZoom();
   const { moveRef } = useMousePosition();
 
-  const { resumeData, setResumeData } = usePublicStore();
+  const {
+    resumeData,
+    isMoving,
+    pageId,
+    attributeIndex,
+    movePageAttribute,
+    setResumeData,
+    setChooseResumeData,
+    setIsMoving,
+    setChooseValue,
+  } = usePublicStore();
 
   useEffect(() => {
     setResumeData(resumeStyle1);
   }, [setResumeData]);
 
-  const setPageAttributeBorder = (attr: PAGE_ATTRIBUTE) => {
-    setResumeData(
-      resumeData.map((page) => {
-        return {
-          ...page,
-          pageAttributes: page.pageAttributes.map((mapAttr) => {
-            let className = mapAttr.className;
-            if (className.includes(" choose_label")) {
-              className = className.replace(" choose_label", "");
-            }
-            if (attr.id === mapAttr.id) {
-              className += " choose_label";
-            }
-            return {
-              ...mapAttr,
-              className,
-            };
-          }),
-        };
-      }),
-    );
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const mouseDownAttribute = (
+    $e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    pid: number,
+    ai: number,
+  ) => {
+    if (pid !== pageId || ai !== attributeIndex) return;
+    setPosition({
+      x: $e.nativeEvent.clientX,
+      y: $e.nativeEvent.clientY,
+    });
+    setChooseValue(pid, ai);
+    setIsMoving(true);
+  };
+
+  const moveChooseAttribute = (
+    $e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    if (!isMoving) return;
+    const stepX = $e.nativeEvent.clientX - position.x;
+    const stepY = $e.nativeEvent.clientY - position.y;
+    movePageAttribute(stepX, stepY);
+    setPosition({
+      x: $e.nativeEvent.clientX,
+      y: $e.nativeEvent.clientY,
+    });
+  };
+
+  const mouseUpAttribute = () => {
+    setIsMoving(false);
+    setPosition({
+      x: 0,
+      y: 0,
+    });
   };
 
   return (
     <div
       className="main-container flex h-full flex-col overflow-auto bg-gray-200"
+      onMouseUp={() => mouseUpAttribute()}
       ref={moveRef}
     >
       <div className="h-full w-full">
@@ -52,20 +79,24 @@ export default function MainContainer() {
             className={`flex flex-col gap-[10] ${styles.print_container}`}
             id="print-container"
             style={{
-              transform: `scale(${scale}) `,
+              transform: `scale(${scale})`,
             }}
           >
-            {resumeData.map((item) => (
+            {resumeData.map((page) => (
               <div
-                className={`relative flex h-[1122px] w-[794px] flex-col justify-between bg-white p-[40px] ${styles.page_container}`}
-                key={item.page}
+                className={`relative flex h-[1122px] w-[794px] flex-col justify-between overflow-hidden bg-white p-[40px] ${styles.page_container}`}
+                key={page.page}
+                onMouseMove={($e) => moveChooseAttribute($e)}
                 style={{ fontSize: "20px" }}
               >
-                {item.pageAttributes.map((attr) => (
+                {page.pageAttributes.map((attr, index) => (
                   <div
                     className={attr.className}
                     key={attr.id}
-                    onClick={() => setPageAttributeBorder(attr)}
+                    onClick={() => setChooseResumeData(attr.id)}
+                    onMouseDown={($e) =>
+                      mouseDownAttribute($e, page.page, index)
+                    }
                     style={attr.style}
                   >
                     {attr.pageLabel}
--- src/lib/store/public.ts
+++ src/lib/store/public.ts
@@ -2,11 +2,135 @@ import { create } from "zustand";
 import type { RESUME_TYPE } from "@/types/resume";
 
 interface PublicState {
+  /**
+   * 页面所有元素
+   */
   resumeData: RESUME_TYPE[];
+  /**
+   * 选中的元素下标
+   */
+  attributeIndex: number;
+  /**
+   * 是否正在移动
+   */
+  isMoving: boolean;
+  /**
+   * 选中的页码,页码为0说明未选中
+   */
+  pageId: number;
+  /**
+   * 设置resumeData值
+   * @param value
+   * @returns
+   */
   setResumeData: (value: RESUME_TYPE[]) => void;
+  /**
+   * 选择页面元素
+   * @param id
+   * @returns
+   */
+  setChooseResumeData: (id: string) => void;
+
+  /**
+   * 设置是否正在移动
+   * @param val
+   * @returns
+   */
+  setIsMoving: (val: boolean) => void;
+
+  /**
+   * 清空选择
+   * @returns
+   */
+  clearChoose: () => void;
+  /**
+   * 移动页面元素
+   * @param pid pageId
+   * @param ai attributeIndex
+   * @returns
+   */
+  setChooseValue: (pid: number, ai: number) => void;
+
+  /**
+   * 移动页面元素
+   * @param pid pageId
+   * @param ai attributeIndex
+   * @param stepX 横向移动距离
+   * @param stepY 纵向移动距离
+   * @returns
+   */
+  movePageAttribute: (stepX: number, stepY: number) => void;
 }
 
 export const usePublicStore = create<PublicState>((set) => ({
+  attributeIndex: 0,
+  clearChoose: () =>
+    set({
+      attributeIndex: 0,
+      pageId: 0,
+    }),
+  isMoving: false,
+  movePageAttribute: (stepX, stepY) =>
+    set((state) => {
+      const { pageId: pid, attributeIndex: ai } = state;
+      const arr = [...state.resumeData];
+      const attrs = arr.find((item) => item.page === pid) as RESUME_TYPE;
+      const { left = "0px", top = "0px" } = attrs.pageAttributes[ai].style;
+      let leftNum = parseInt(left?.toString(), 10);
+      let topNum = parseInt(top?.toString(), 10);
+
+      if (topNum <= 40) topNum = 40;
+      if (leftNum <= 40) leftNum = 40;
+
+      attrs.pageAttributes[ai].style = {
+        ...attrs.pageAttributes[ai].style,
+        left: `${leftNum + stepX}px`,
+        top: `${topNum + stepY}px`,
+        zIndex: 99,
+      };
+
+      return { resumeData: arr };
+    }),
+  pageId: 0,
   resumeData: [],
-  setResumeData: (value) => set({ resumeData: value }),
+  setChooseResumeData: (id: string) =>
+    set((state) => {
+      return {
+        isChooseAttributes: true,
+        resumeData: state.resumeData.map((page) => {
+          return {
+            ...page,
+            pageAttributes: page.pageAttributes.map((mapAttr, index) => {
+              let className = mapAttr.className;
+              const style = { ...mapAttr.style };
+
+              if (style?.zIndex) {
+                style.zIndex = 0;
+              }
+
+              if (className.includes(" choose_label")) {
+                className = className.replace(" choose_label", "");
+              }
+              if (id === mapAttr.id) {
+                state.pageId = page.page;
+                state.attributeIndex = index;
+                className += " choose_label";
+              }
+              return {
+                ...mapAttr,
+                className,
+                style,
+              };
+            }),
+          };
+        }),
+      };
+    }),
+  setChooseValue: (pid, ai) =>
+    set({
+      attributeIndex: ai,
+      pageId: pid,
+    }),
+  setIsMoving: (val: boolean) => set({ isMoving: val }),
+  setResumeData: (value: RESUME_TYPE[]) => set({ resumeData: value }),
 }));
