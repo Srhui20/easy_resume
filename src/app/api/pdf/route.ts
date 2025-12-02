9450cd8e7dfd70bdcffcf72c8de0a2efabbb7ba0
Srhui20 <2874744820@qq.com>
2025-12-02 09:56:53 +0800

    feat: 测试Vercel服务导出pdf

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,5 +1,36 @@
 import { type NextRequest, NextResponse } from "next/server";
+import chromium from "@sparticuz/chromium";
 import puppeteer from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+
+export async function getBrowser() {
+  const isVercel = !!process.env.VERCEL; // Vercel 内置环境变量
+  const isLinuxServer = process.platform === "linux" && !isVercel;
+
+  if (isVercel) {
+    // 运行在 Vercel（Serverless）
+    return puppeteerCore.launch({
+      args: chromium.args,
+      // defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      // headless: chromium.headless,
+    });
+  }
+
+  if (isLinuxServer) {
+    // 运行在 Linux 服务器（比如宝塔、ECS）
+    return puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+  }
+
+  // 本地开发
+  return puppeteer.launch({
+    headless: true,
+    args: [],
+  });
+}
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,10 +40,7 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ code: 400, message: "html 不能为空" });
     }
 
-    // 启动 Puppeteer
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"], // Linux 部署推荐
-    });
+    const browser = await getBrowser();
     const page = await browser.newPage();
 
     // 设置页面内容
@@ -23,7 +51,7 @@ export async function POST(req: NextRequest) {
         </head>
         <body>${html}</body>
       </html>`,
-      { waitUntil: "networkidle0" },
+      { waitUntil: "networkidle0" }
     );
 
     // 生成 PDF
