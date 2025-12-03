c85cc6568b69326f2794bd6b5f342df0fc8b06d6
Srhui20 <2874744820@qq.com>
2025-12-03 13:14:49 +0800

    Revert "feat: 测试Vercel服务导出pdf"
    This reverts commit 9450cd8e7dfd70bdcffcf72c8de0a2efabbb7ba0.

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,36 +1,5 @@
-import chromium from "@sparticuz/chromium";
 import { type NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer";
-import puppeteerCore from "puppeteer-core";
-
-export async function getBrowser() {
-  const isVercel = !!process.env.VERCEL; // Vercel 内置环境变量
-  const isLinuxServer = process.platform === "linux" && !isVercel;
-
-  if (isVercel) {
-    // 运行在 Vercel（Serverless）
-    return puppeteerCore.launch({
-      args: chromium.args,
-      // defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
-      // headless: chromium.headless,
-    });
-  }
-
-  if (isLinuxServer) {
-    // 运行在 Linux 服务器（比如宝塔、ECS）
-    return puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: true,
-    });
-  }
-
-  // 本地开发
-  return puppeteer.launch({
-    args: [],
-    headless: true,
-  });
-}
 
 export async function POST(req: NextRequest) {
   try {
@@ -40,7 +9,10 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ code: 400, message: "html 不能为空" });
     }
 
-    const browser = await getBrowser();
+    // 启动 Puppeteer
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"], // Linux 部署推荐
+    });
     const page = await browser.newPage();
 
     // 设置页面内容
@@ -51,7 +23,7 @@ export async function POST(req: NextRequest) {
         </head>
         <body>${html}</body>
       </html>`,
-      { waitUntil: "networkidle0" },
+      { waitUntil: "networkidle0" }
     );
 
     // 生成 PDF
