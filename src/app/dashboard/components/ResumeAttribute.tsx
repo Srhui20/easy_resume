ddc1da743fa6a778f33de9c8658d775a852d4e01
Srhui20 <2874744820@qq.com>
2025-12-04 22:39:50 +0800

    feat: 初始化普通文本的基本属性

--- src/app/dashboard/components/ResumeAttribute.tsx
+++ src/app/dashboard/components/ResumeAttribute.tsx
@@ -1,3 +1,141 @@
+import {
+  BoldOutlined,
+  ItalicOutlined,
+  UnderlineOutlined,
+} from "@ant-design/icons";
+import { ColorPicker, Input, InputNumber } from "antd";
+import { useEffect, useState } from "react";
+import { usePublicStore } from "@/lib/store/public";
+import type { PAGE_ATTRIBUTE } from "@/types/resume";
+
 export default function ResumeAttribute() {
-  return <div className="h-full w-full">fas</div>;
+  const { resumeData, pageId, attributeIndex } = usePublicStore();
+  const [isBaseInfo, setIsBaseInfo] = useState(false);
+  const [currentNode, setCurrentNode] = useState<PAGE_ATTRIBUTE | null>(null);
+
+  useEffect(() => {
+    if (!pageId) return;
+    const cNode =
+      resumeData.find((item) => item.page === pageId)?.pageAttributes[
+        attributeIndex
+      ] ?? null;
+
+    setCurrentNode(() => cNode);
+    setIsBaseInfo(() => cNode?.type === "baseInfo");
+  }, [pageId, attributeIndex, resumeData]);
+
+  const fontStylesList = [
+    {
+      handlerFun: () => {},
+      icon: <BoldOutlined />,
+      isChoose: true,
+      key: "blod",
+      label: "加粗",
+    },
+    {
+      handlerFun: () => {},
+      icon: <ItalicOutlined />,
+      isChoose: false,
+      key: "italics",
+      label: "斜体",
+    },
+    {
+      handlerFun: () => {},
+      icon: <UnderlineOutlined />,
+      isChoose: false,
+      key: "underline",
+      label: "下划线",
+    },
+  ];
+
+  return (
+    <div className="h-full w-full">
+      {isBaseInfo ? (
+        <div className="flex h-full w-full flex-col pt-[10px] pb-[10px] text-[16px]">
+          {/* 文本 */}
+          <div className="mb-[20px] flex flex-col">
+            <div className="mb-[10px] text-gray-600">文本</div>
+            <Input
+              size="large"
+              style={{ fontSize: "16px", height: "50px" }}
+              value={currentNode?.pageLabel}
+            />
+          </div>
+          {/* 字体大小和颜色 */}
+          <div className="mb-[20px] flex w-full justify-between gap-[80px]">
+            <div className="flex flex-1 flex-col">
+              <div className="mb-[10px] text-gray-600">大小</div>
+              <div className="flex w-full justify-center">
+                <InputNumber
+                  className="flex-1"
+                  size="large"
+                  style={{ fontSize: "16px", height: "50px", width: "100%" }}
+                  value={12}
+                />
+                <div className="ml-[5px] flex items-center">px</div>
+              </div>
+            </div>
+            <div className="flex flex-1 flex-col">
+              <div className="mb-[10px] text-gray-600">颜色</div>
+              <ColorPicker
+                defaultValue="#1677ff"
+                showText
+                style={{
+                  alignItems: "center",
+                  fontSize: "16px",
+                  height: "50px",
+                  justifyContent: "start",
+                  paddingLeft: "20px",
+                  width: "100%",
+                }}
+              />
+            </div>
+          </div>
+          {/* 位置 */}
+          <div className="mb-[20px] flex w-full justify-between gap-[80px]">
+            <div className="flex flex-1 flex-col">
+              <div className="mb-[10px] text-gray-600">X轴</div>
+              <div className="flex w-full justify-center">
+                <InputNumber
+                  className="flex-1"
+                  size="large"
+                  style={{ fontSize: "16px", height: "50px", width: "100%" }}
+                  value={12}
+                />
+                <div className="ml-[5px] flex items-center">px</div>
+              </div>
+            </div>
+            <div className="flex flex-1 flex-col">
+              <div className="mb-[10px] text-gray-600">Y轴</div>
+              <div className="flex w-full justify-center">
+                <InputNumber
+                  className="flex-1"
+                  size="large"
+                  style={{ fontSize: "16px", height: "50px", width: "100%" }}
+                  value={12}
+                />
+                <div className="ml-[5px] flex items-center">px</div>
+              </div>
+            </div>
+          </div>
+          {/* 样式 */}
+          <div className="flex h-[50px] items-center justify-between">
+            <div className="flex-1 text-gray-600">样式</div>
+            <div className="flex h-full justify-center gap-[4px] rounded-lg bg-gray-100 p-[3px] pr-[8px] pl-[8px]">
+              {fontStylesList.map((item) => (
+                <div
+                  className={`flex w-[36px] cursor-pointer items-center justify-center rounded-lg ${item.isChoose ? "bg-blue-400 text-white" : "hover:bg-gray-300"}`}
+                  key={item.key}
+                >
+                  {item.icon}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>123</div>
+      )}
+    </div>
+  );
 }
