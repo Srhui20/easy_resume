51f0f9465963f1d32c723cd45c8f73ee7b6e265b
Srhui20 <2874744820@qq.com>
2025-12-10 17:56:39 +0800

    feat: 初始化ai

---
+++ src/app/api/ai/route.ts
@@ -0,0 +1,76 @@
+import { type NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+// const DEEPSEEK_API_ENDPOINT = "https://api.deepseek.com/v1/chat/completions";
+const DEEPSEEK_API_KEY = process.env.DEEPSEEK_API_KEY;
+
+// PAGE_ATTRIBUTE = {
+//   className: string;
+//   id: string;
+//   pageLabel?: string;
+//   style: CSSProperties;
+//   ref?: HTMLDivElement | null;
+//   type: "baseInfo" | "paragraph";
+//   titleInfo?: {
+//     label: string;
+//     style: CSSProperties;
+//   };
+//   paragraphArr?: PARAGRAPH_TYPE[];
+//   borderStyle?: CSSProperties;
+// };
+
+const systemPrompt = `
+      输入数据是一个对象数组，主要包含两种类型元素：
+      普通文本 (type: 'label')**: 包含 pageLabel (文本内容) 和className(样式信息，需忽略)。
+      段落/模块 (type: 'paragraph')**: 包含 titleInfo.label(段落标题，如"工作经验") 和 paragraphArr (具体项目数组)。
+      paragraphArr数组中的每个项目代表一项经验或成就，其关键字段包括：
+      name: 主体名称（如公司名、项目名）。
+      position: 担任的职位或角色。
+      startTime和endTime: 时间范围。
+      label: 段落的具体描述内容。
+      从以下几个方面进行详细点评：
+      1. 格式和排版（清晰度，专业性）。
+      2. 个人亮点和成就。
+      3. 经验描述。
+      4. 改进建议（提供具体、可操作的修改建议）。
+      最重要的是输出文本要加入小图标
+      请使用中文，并以 Markdown 格式清晰地输出结果。
+    `;
+
+const openai = new OpenAI({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  baseURL: "https://api.deepseek.com",
+});
+
+export async function POST(req: NextRequest) {
+  if (!DEEPSEEK_API_KEY) {
+    return NextResponse.json({ code: 500, message: "No Key!" });
+  }
+
+  const { dataString } = await req.json();
+
+  // 3. 调用 DeepSeek API
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        content: systemPrompt,
+        role: "system",
+      },
+      {
+        content: dataString,
+        role: "user",
+      },
+    ],
+    model: "deepseek-chat", // 使用 DeepSeek 的聊天模型
+    stream: true, // 暂不使用流式传输，简化处理
+  });
+
+  return new Response(completion.toReadableStream(), {
+    headers: {
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+      "Content-Type": "text/event-stream",
+    },
+  });
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { DownloadOutlined, GithubOutlined } from "@ant-design/icons";
-import { Button, message, Splitter, Tooltip } from "antd";
-import { useEffect } from "react";
+import {
+  DownloadOutlined,
+  GithubOutlined,
+  OpenAIOutlined,
+} from "@ant-design/icons";
+import { Button, FloatButton, Modal, message, Splitter, Tooltip } from "antd";
+import { marked } from "marked";
+import { useEffect, useState } from "react";
 import { usePublicStore } from "@/lib/store/public";
 import type { OperationBtnType } from "@/types/resume";
 import MainContainer from "./components/MainContainer";
@@ -10,7 +15,7 @@ import RightInfo from "./components/RightInfo";
 
 export default function Dashboard() {
   const [messageApi, contextHolder] = message.useMessage();
-
+  const [aiMessages, setAiMessages] = useState("sadf");
   const resumeData = usePublicStore((state) => state.resumeData);
 
   useEffect(() => {
@@ -70,44 +75,142 @@ export default function Dashboard() {
     URL.revokeObjectURL(url);
   };
 
+  const getAiEvaluate = async () => {
+    try {
+      const res = await fetch("/api/ai", {
+        body: JSON.stringify({
+          dataString: JSON.stringify(
+            resumeData.map((item) => {
+              return {
+                ...item,
+                ref: null,
+              };
+            }),
+          ),
+        }),
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+      });
+      const reader = res.body?.getReader();
+      if (!reader) return;
+
+      setAiMessages("");
+      const decoder = new TextDecoder();
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const chunk = decoder.decode(value);
+        const lines = chunk.split("\n");
+        for (const line of lines) {
+          if (!line) continue;
+          const data = JSON.parse(line || "{}");
+          data?.choices?.forEach((item: { delta: { content: string } }) => {
+            setAiMessages(
+              (prevMessages) => prevMessages + (item?.delta?.content ?? ""),
+            );
+          });
+        }
+      }
+
+      localStorage.setItem("ai", aiMessages);
+    } catch (e) {
+      // biome-ignore lint/suspicious/noConsole: <explanation>
+      console.error("ai error", e);
+    }
+  };
+  const [modalOpen, setOpen] = useState(false);
+  const openAiDialog = () => {
+    setAiMessages(localStorage.getItem("ai") || "");
+    setOpen(true);
+  };
+
+  const footer: React.ReactNode = (
+    <>
+      <Button
+        onClick={() => setOpen(false)}
+        styles={{
+          root: {
+            backgroundColor: "#fff",
+            borderColor: "#ccc",
+            color: "#171717",
+          },
+        }}
+      >
+        Cancel
+      </Button>
+      <Button
+        onClick={() => getAiEvaluate()}
+        styles={{ root: { backgroundColor: "#171717" } }}
+        type="primary"
+      >
+        Submit
+      </Button>
+    </>
+  );
+
   return (
-    <div className="flex h-screen flex-col">
-      {contextHolder}
-      <div className="flex h-15 items-center justify-between border-gray-300 border-b pr-8 pl-8">
-        <div className="font-bold">EASY_RESUME</div>
-        <div className="flex gap-3">
-          {btnList.map((btn) => (
-            <Button
-              icon={btn.icon}
-              key={btn.key}
-              onClick={() => btn.handleFunc()}
-              type={btn.type}
-            >
-              {btn.label}
-            </Button>
-          ))}
-          <Tooltip title="Github">
-            <Button
-              href="https://github.com/Srhui20/easy_resume"
-              icon={<GithubOutlined />}
-              style={{ fontSize: "16px" }}
-              type="text"
-            />
-          </Tooltip>
+    <>
+      <Modal
+        centered={true}
+        footer={footer}
+        onCancel={() => setOpen(false)}
+        onOk={() => setOpen(false)}
+        open={modalOpen}
+        title="ai点评"
+      >
+        <div
+          className="h-[400px] overflow-auto bg-white"
+          dangerouslySetInnerHTML={{
+            __html: marked(aiMessages),
+          }}
+        />
+      </Modal>
+      <div className="flex h-screen flex-col">
+        {contextHolder}
+        <div className="flex h-15 items-center justify-between border-gray-300 border-b pr-8 pl-8">
+          <div className="font-bold">EASY_RESUME</div>
+          <div className="flex gap-3">
+            {btnList.map((btn) => (
+              <Button
+                icon={btn.icon}
+                key={btn.key}
+                onClick={() => btn.handleFunc()}
+                type={btn.type}
+              >
+                {btn.label}
+              </Button>
+            ))}
+            <Tooltip title="Github">
+              <Button
+                href="https://github.com/Srhui20/easy_resume"
+                icon={<GithubOutlined />}
+                style={{ fontSize: "16px" }}
+                type="text"
+              />
+            </Tooltip>
+          </div>
         </div>
+        <div className="flex min-h-0 flex-1">
+          <Splitter
+            style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", height: "100%" }}
+          >
+            <Splitter.Panel defaultSize="70%" max="70%" min="50%">
+              <MainContainer />
+            </Splitter.Panel>
+            <Splitter.Panel>
+              <RightInfo />
+            </Splitter.Panel>
+          </Splitter>
+        </div>
+        <FloatButton
+          icon={<OpenAIOutlined />}
+          onClick={() => openAiDialog()}
+          tooltip="ai点评"
+          type="primary"
+        />
+        ;
       </div>
-      <div className="flex min-h-0 flex-1">
-        <Splitter
-          style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", height: "100%" }}
-        >
-          <Splitter.Panel defaultSize="70%" max="70%" min="50%">
-            <MainContainer />
-          </Splitter.Panel>
-          <Splitter.Panel>
-            <RightInfo />
-          </Splitter.Panel>
-        </Splitter>
-      </div>
-    </div>
+    </>
   );
 }
