4096eaa83209b528b3969b5d3ceb54aabb9e4333
Srhui20 <2874744820@qq.com>
2025-12-16 16:33:01 +0800

    feat: 测试ioredis

--- src/lib/redis/index.ts
+++ src/lib/redis/index.ts
@@ -1,6 +1,19 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL as string);
+export const redis = new Redis(process.env.REDIS_URL as string, {
+  enableReadyCheck: false,
+  maxRetriesPerRequest: 3,
+  reconnectOnError(err) {
+    const targetError = "READONLY";
+    if (err.message.includes(targetError)) {
+      return true;
+    }
+    return false;
+  },
+  retryStrategy(times) {
+    return Math.min(times * 50, 2000);
+  },
+});
 
 redis.on("connect", () => {
   // biome-ignore lint/suspicious/noConsole: <explanation>
