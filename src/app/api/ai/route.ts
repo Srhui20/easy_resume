1607f0f5511072f7a6e688352a20661debd3e464
Srhui20 <2874744820@qq.com>
2025-12-19 10:17:13 +0800

    feat: 改进systemPrompt

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -6,8 +6,8 @@ import { checkIpLimit } from "@/lib/redis/rateLimit";
 const DEEPSEEK_API_KEY = process.env.DEEPSEEK_API_KEY;
 
 const systemPrompt = `
-      输入数据是一个对象数组，主要包含两种类型元素：
-      段落/模块 (type: 'paragraph')**: 包含 titleInfo.label(段落标题，如"工作经验") 和 paragraphArr (具体项目数组)。
+      输入数据是一个对象数组：
+      包含 titleInfo.label(段落标题，如"工作经验") 和 paragraphArr (具体项目数组)。
       paragraphArr数组中的每个项目代表一项经验或成就，其关键字段包括：
       name: 主体名称（如公司名、项目名）。
       position: 担任的职位或角色。
@@ -16,11 +16,11 @@ const systemPrompt = `
       从以下几个方面进行详细点评：
       1. 格式和排版（清晰度，专业性）。
       2. 个人亮点和成就。
-      3. 经验描述。
+      3. 工作和项目经验描述。
       4. 改进建议（提供具体、可操作的修改建议）。
       5. 给出具体的改正后的文本。
       不需要点评页面上的style及className,点评实际显示效果即可，
-      输出文本要多加入小图标，
+      输出文本要多加入小图标，输出所有的内容不要输出数组或对象格式，都以正常显示结果展示。
       请使用中文，并以 Markdown 格式清晰地输出结果，markdown格式一定要正确，
       输出的markdown不能超过550px(最重要)
     `;
